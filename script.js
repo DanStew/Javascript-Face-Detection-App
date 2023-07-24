ebaf6ea2ac52e21258e38dcd5928c3e6f6c5306d
DanStew <90531180+DanStew@users.noreply.github.com>
2023-07-24 13:50:35 +0100

    Adding missing files, loading files and start cam

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //Using the face-api.js library to built a face recognition app that uses your webcam
 
+//Accessing the video element of the page
+const video = document.getElementById("video")
+
+//Loading the faceapi models
+
+Promise.all([
+    //Allows it to work on small faces, speeds up the recognition model
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    //Recognises the key landmarks of your face
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    //Recognises the outline of the face
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    //Able to recognise face exspressions
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo())
+
+function startVideo(){
+    //Code to get the users webcam
+    navigator.getUserMedia(
+        //Video is the parameter that you are passing into this function
+        { video: {} },
+        //Stream runs if you are able to access video
+        stream => video.srcObject = stream,
+        //Otherwise the error function will run
+        err => console.error(err))
+}
