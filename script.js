7778c92a732d83efab54aba6d63f40ecf7a37535
DanStew <90531180+DanStew@users.noreply.github.com>
2023-07-24 14:15:41 +0100

    Completed App

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const video = document.getElementById("video")
 
 //Loading the faceapi models
-
+//Promise.all ensures that everything is loaded before continuing code
 Promise.all([
     //Allows it to work on small faces, speeds up the recognition model
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -16,6 +16,7 @@ Promise.all([
     faceapi.nets.faceExpressionNet.loadFromUri('/models'),
 ]).then(startVideo())
 
+//Function used to start the webcam and display it onto the website
 function startVideo(){
     //Code to get the users webcam
     navigator.getUserMedia(
@@ -26,3 +27,39 @@ function startVideo(){
         //Otherwise the error function will run
         err => console.error(err))
 }
+
+//Adding an event listener for when the video starts playing
+video.addEventListener("play", () => {
+
+    //Creating a canvas to display the markings onto the website and adding it to the website
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+
+    //Collecting the display size of the video on the website, so you can add detections over it
+    const displaySize = {width : video.width, height : video.height}
+
+    //Matching canvas size to display size
+    faceapi.matchDimensions(canvas,displaySize)
+
+    //Function to make the code here run multiple times
+    //Function needs to be async as faceapi is an asyncronis function (Spelt Wrong)
+    setInterval(async () => {
+        //Parameters here are what video you want to use, and what you are using to detect the faces
+        //Including all of the different detection algorithms that you want to include
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+
+        //Resizing the detections to be the correct display size of the video
+        const resizedDetections = faceapi.resizeResults(detections,displaySize)
+
+        //Clearing the old canvas
+        canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
+
+        //Drawing the resized detections to the screen
+        faceapi.draw.drawDetections(canvas,resizedDetections)
+
+        //Drawing additional details and features
+        //Both these lines of code can add additional elements to the face, can cause it to lag though
+        //faceapi.draw.drawFaceLandmarks(canvas,resizedDetections)
+        //faceapi.draw.drawFaceExpressions(canvas,resizedDetections)
+    },300)
+})
